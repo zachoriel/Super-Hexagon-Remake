c6bd109e5120d407ff962cdc54c6f81459b2feef
Zach Oriel <s179065@aieeduau.onmicrosoft.com>
2018-10-14 01:15:49 -0700

    Changed background color, added music, implemented mobile input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,28 +7,70 @@ public class PlayerController : MonoBehaviour
     [Header("Editor Setup")]
     public GameObject gameOverPanel;
     public Animator animator;
+    SoundManager sounds;
 
     [Header("Player Settings")]
     [SerializeField] float moveSpeed = 600f;
 
     float movement = 0f;
 
-	// Update is called once per frame
-	void Update ()
+    bool leftPointer, rightPointer;
+
+    void Start()
+    {
+        sounds = FindObjectOfType<SoundManager>();
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         movement = Input.GetAxisRaw("Horizontal");
-	}
+
+        if (leftPointer == true)
+        {
+            movement = -1f;
+        }
+
+        if (rightPointer == true)
+        {
+            movement = 1f;
+        }
+    }
 
     void FixedUpdate()
     {
-        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+        if (movement != 0)
+        {
+            transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+        }
     }
+
+    #region MobilePointerEvents
+    public void PointerDownLeft()
+    {
+        leftPointer = true;
+    }
+    public void PointerUpLeft()
+    {
+        leftPointer = false;
+    }
+    public void PointerDownRight()
+    {
+        rightPointer = true;
+    }
+    public void PointerUpRight()
+    {
+        rightPointer = false;
+    }
+    #endregion
 
     void DisplayGameOver()
     {
         Time.timeScale = 0f;
         gameOverPanel.SetActive(true);
         animator.SetTrigger("GameOver");
+
+        sounds.GameOver();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip technoLoop, gameOver;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        MusicLoop();
+	}
+
+    public void MusicLoop()
+    {
+        audioSource.clip = technoLoop;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    public void GameOver()
+    {
+        audioSource.Stop();
+        audioSource.clip = gameOver;
+        audioSource.loop = false;
+        audioSource.Play();
+    }
+}
