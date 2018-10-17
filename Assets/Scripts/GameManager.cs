ae03bdcbeb1c0a30e8da9b0d5df36a7da5990f00
Zach Oriel <Zacharyaaron15@gmail.com>
2018-10-17 15:44:18 -0700

    Colors

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Camera mainCam;
+
+	// Use this for initialization
+	void Start ()
+    {
+		if (mainCam == null)
+        {
+            mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        }
+
+        StartCoroutine(ChangeBackground());
+	}
+
+    IEnumerator ChangeBackground()
+    {
+        while (true)
+        {
+            mainCam.backgroundColor = new Color(Random.value, Random.value, Random.value, 0f);
+            yield return new WaitForSeconds(5f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
